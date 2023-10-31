3e91331483d5ae54040ce8b3c43c3c93bb56c55e
Kjetil Olsen Lye <kjetil.olsen.lye@sintef.no>
2023-10-31 20:21:48 +0100

    added two qois

--- python/get_samples.py
+++ python/get_samples.py
@@ -1,21 +1,41 @@
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
+from tqdm import tqdm
+import csv
 
-sys.path.append('/home/kjetil/projects/across/ensemble_pavel/opm/opm/build/opm-common/python')
+sys.path.append('/home/kjetil/projects/sintef/across/ensemble_pavel/opm/opm/build/opm-common/python')
+base_filename_grid = "/mnt/data_internal/kjetil/pavel/output/output/sleipner_decks/Coarse_Sleipner_ensemble/Coarse_Sleipner_ensemble_{ensemble_number:04d}/output/COARSE_SLEIPNER_ENSEMBLE_{ensemble_number:04d}.EGRID"
+
 import opm
 from opm.io.ecl import ESmry, EGrid,EclOutput,EclFile,ERft
 
-base_filename = "/home/kjetil/projects/across/ensemble_pavel/mrst/mrst-bitbucket/mrst-core/output/sleipner_decks/Coarse_Sleipner_ensemble/Coarse_Sleipner_ensemble_{ensemble_number}/mpi/14/dev.5/COARSE_SLEIPNER_ENSEMBLE_{ensemble_number}.UNRST"
+base_filename = "/mnt/data_internal/kjetil/pavel/output/output/sleipner_decks/Coarse_Sleipner_ensemble/Coarse_Sleipner_ensemble_{ensemble_number:04d}/output/COARSE_SLEIPNER_ENSEMBLE_{ensemble_number:04d}.UNRST"
 ensemble_numbers = list(range(1, 513))
-qois = np.zeros(len(ensemble_numbers))
-for ensemble_number in ensemble_numbers:
-    ensemble_number_formatted = f'{ensemble_number:04d}'
-    ecloutput = EclFile(base_filename.format(ensemble_number = ensemble_number_formatted))
-    sgasmean = np.mean(ecloutput['PRESSURE'])
-    qois[ensemble_number-1] = sgasmean
-plt.hist(qois)
-plt.xlabel("mean of PRESSURE across all grid cells")
-plt.ylabel("Occurences")
-                        
-    
+
+
+qoi_functions = {
+    "moveable_co2" : lambda ecl, egrid: np.mean(egrid.cellvolumes() * ecl['SGAS'] * (ecl['SGAS']>0.1)),
+    "pressure": lambda ecl, egrid: np.mean(egrid.cellvolumes() * ecl['PRESSURE'])
+}
+
+for qoi_name, qoi_function in qoi_functions.items():
+    qois = np.zeros(len(ensemble_numbers))
+    for ensemble_number in tqdm(ensemble_numbers):
+        ensemble_number_formatted = f'{ensemble_number:04d}'
+        ecloutput = EclFile(base_filename.format(ensemble_number = ensemble_number))
+        egrid = EGrid(base_filename_grid.format(ensemble_number=ensemble_number))
+        qoi = qoi_function(ecloutput, egrid)#np.mean(cellvolums * ecloutput['SGAS'] * (ecloutput['SGAS']>0.1))
+        qois[ensemble_number-1] = qoi
+    plt.hist(qois)
+    plt.xlabel(f"{qoi_name} across all grid cells")
+    plt.ylabel("Occurences")
+    plt.show()
+
+    with open(f'{qoi_name}.csv', 'w', newline='') as csvfile:
+        fieldnames = ['sample', qoi_name]
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+        writer.writeheader()
+        for n, qoi in enumerate(tqdm(qois)):
+            writer.writerow({'sample' : n+1, qoi_name:qoi})
